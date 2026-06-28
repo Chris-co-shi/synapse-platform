7e19749e51c81e33666053da659101351a256141
Chris Shi <christ.sxc@gmail.com>
2026-06-28 10:58:59 +0800

    refactor(iam): clean credential entity imports

--- synapse-iam-platform/synapse-iam-server/src/main/java/com/indigo/synapse/iam/identity/infrastructure/persistence/entity/IamUserCredentialEntity.java
+++ synapse-iam-platform/synapse-iam-server/src/main/java/com/indigo/synapse/iam/identity/infrastructure/persistence/entity/IamUserCredentialEntity.java
@@ -1,13 +1,8 @@
 package com.indigo.synapse.iam.identity.infrastructure.persistence.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.Version;
 import com.indigo.synapse.iam.identity.domain.model.UserCredential;
-import com.indigo.synapse.mybatisplus.entity.ManagedEntity;
 import com.indigo.synapse.mybatisplus.entity.VersionedEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
